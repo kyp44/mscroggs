6bd0e049f678bd864e9fef8fcc480b604563acf6
Dan Whitman <dwhitman44@mgail.com>
2023-12-13 07:44:04 -0500

    Transfer. Adds only the questions for the 2023 Dec 7 through Dec 13 problems.

---
+++ advent_2023/dec11.py
@@ -0,0 +1,14 @@
+import numpy as np
+from common import *
+
+sol = np.array([[1, 1, 1],
+                [1, 1, 1],
+                [1, 1, 1]])
+
+box_solutions(
+    2023,
+    11,
+    sol,
+    product,
+    False,
+)
