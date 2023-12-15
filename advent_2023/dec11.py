7a6cb905e22c8a9a665a5f355c99e03a01c69408
Dan Whitman <daniel.whitman.2@us.af.mil>
2023-12-15 16:59:06 -0500

    Solves the 2023, Dec 11 box puzzle.

--- advent_2023/dec11.py
+++ advent_2023/dec11.py
@@ -1,14 +1,14 @@
 import numpy as np
 from common import *
 
-sol = np.array([[1, 1, 1],
-                [1, 1, 1],
-                [1, 1, 1]])
+sol = np.array([[3, 7, 5],
+                [9, 2, 1],
+                [4, 8, 6]])
 
 box_solutions(
     2023,
     11,
     sol,
     product,
-    False,
+    True,
 )
