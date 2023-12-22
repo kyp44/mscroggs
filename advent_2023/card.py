908be719ba20aae31a27868fa7d6e1b4f0692481
Dan Whitman <dwhitman44@gmail.com>
2023-12-22 18:18:29 -0500

    Transfer. Reworks the 2023 Card, Problem 7 and 8, as a more general solution was discovered. Starts working on the 2023, Dec 22 problem, but the current solution is not correct.

--- advent_2023/card.py
+++ advent_2023/card.py
@@ -9,27 +9,26 @@ pans(factorial_minus(1))
 prob(2)
 pans(factorial_minus(44))
 
+
+def prob7_brute(d, k, n):
+    valid_digs = set(range(1, d+1))
+    cnt = 0
+    for i in range(10**(n-1), 10**n):
+        ds = digits(i)
+        if set(ds).issubset(valid_digs):
+            if len([x for x in ds if x == 1]) == (n-k):
+                cnt += 1
+    return cnt
+
+
 prob(7)
-# n-digit numbers
-n = 3
-# Number of digits to allow
-d = 5
-valid_digs = set(range(1, d+1))
-cnt = 0
-for k in range(10**(n-1), 10**n):
-    ds = digits(k)
-    if set(ds).issubset(valid_digs):
-        if len([x for x in ds if x == 1]) == n-1:
-            cnt += 1
+d, k, n = (5, 1, 3)
+print("Formula:", binom(n, k)*(d-1)**k)
+pans(prob7_brute(d, k, n))
 
-
-def N(n):
-    if n == 1:
-        return d-1
-    else:
-        return d-1 + N(n-1)
-
-
-print("Recursive:", N(n))
-print("Formulat:", n*(d-1))
-pans(cnt)
+prob(8)
+d, k, n = (5, 1, 75)
+print("Verification:", binom(n, k)*(d-1)**k)
+# Takes too long
+print("Brute force verification:", prob7_brute(d, k, n))
+pans(300//4)
