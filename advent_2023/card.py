107c0bd38a7b1bdce0379579c2ac49a43da27c7b
Dan Whitman <dwhitman44@gmail.com>
2023-12-21 22:38:53 -0500

    Completes the 2023 Christmas Card, Problems 5-8. Also adds the 2018, Dec 2 problem.

--- advent_2023/card.py
+++ advent_2023/card.py
@@ -8,3 +8,28 @@ pans(factorial_minus(1))
 
 prob(2)
 pans(factorial_minus(44))
+
+prob(7)
+# n-digit numbers
+n = 3
+# Number of digits to allow
+d = 5
+valid_digs = set(range(1, d+1))
+cnt = 0
+for k in range(10**(n-1), 10**n):
+    ds = digits(k)
+    if set(ds).issubset(valid_digs):
+        if len([x for x in ds if x == 1]) == n-1:
+            cnt += 1
+
+
+def N(n):
+    if n == 1:
+        return d-1
+    else:
+        return d-1 + N(n-1)
+
+
+print("Recursive:", N(n))
+print("Formulat:", n*(d-1))
+pans(cnt)
