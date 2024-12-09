4c83a667fc7a5b64f4a8547afd449c61bf46589e
Dan Whitman <daniel.whitman.2@us.af.mil>
2024-12-09 12:29:54 -0500

    Completes the 2024 Dec 8 and Dec 9 problems.

--- advent_2024/dec09.py
+++ advent_2024/dec09.py
@@ -1,13 +1,13 @@
 import numpy as np
 from common import *
 
-sol = np.array([[2, 6, 7],
-                [4, 3, 8],
-                [9, 5, 1]])
+sol = np.array([[7, 8, 9],
+                [5, 4, 1],
+                [6, 2, 3]])
 
 box_solutions(
     2024,
-    5,
+    9,
     sol,
     product,
     True,
